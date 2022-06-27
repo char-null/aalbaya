87bafd3db49f394a0c7d84f112b160ac1b4d6ac4
char-null <wool999999@gmail.com>
2022-06-27 22:16:30 +0900

    style 추가

--- lib/src/styles/textstyle.dart
+++ lib/src/styles/textstyle.dart
@@ -1,8 +1,13 @@
 import 'package:aalbaya/src/styles/colors.dart';
 import 'package:flutter/material.dart';
 
-const headstyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+const headstyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
 
-const contentstyle = TextStyle(fontSize: 14);
+const subheadstyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
+const contentstyle = TextStyle(fontSize: 16, color: Colors.black);
+
+const contentpointstyle =
+    TextStyle(fontSize: 15, color: pointcolor, fontWeight: FontWeight.bold);
 
 const pointstyle = TextStyle(fontWeight: FontWeight.bold, color: pointcolor);
