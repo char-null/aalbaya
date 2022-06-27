b0ee6674f2e69a9179f7f0e1a60dc16372321423
char-null <wool999999@gmail.com>
2022-06-27 13:19:49 +0900

    textstyle 추가

---
+++ lib/src/styles/textstyle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const TextStyle headstyle =
+    TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
+const TextStyle contentstyle = TextStyle(
+  fontSize: 14,
+);
