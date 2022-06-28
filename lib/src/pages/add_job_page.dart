9dca40d3c29a760a4d2af944ebac4793ce3d6625
char-null <wool999999@gmail.com>
2022-06-28 22:03:42 +0900

    formfield 생성

--- lib/src/pages/add_job_page.dart
+++ lib/src/pages/add_job_page.dart
@@ -1,22 +1,23 @@
-import 'package:aalbaya/src/controller/taskcontroller.dart';
+import 'package:aalbaya/src/controller/jobcontroller.dart';
 import 'package:aalbaya/src/styles/colors.dart';
 import 'package:aalbaya/src/styles/textstyle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class AddTaskPage extends StatelessWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _ = Get.put(TaskController());
+    final _ = Get.put(JobController());
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: GetBuilder<TaskController>(builder: (_) {
+      child: GetBuilder<JobController>(builder: (_) {
         return Scaffold(
           backgroundColor: backgroundcolor,
           appBar: AppBar(
@@ -35,205 +36,64 @@ class AddTaskPage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(
                 horizontal: 18.0,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    '직종 선택',
-                    style: headstyle,
-                  ),
-                  empty(),
-                  SizedBox(
-                    height: 50,
-                    child: GridView(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        childAspectRatio: 2 / 1,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 10,
-                      ),
+              child: FormBuilder(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    choiceJob(),
+                    textFormWidget('jobname', '직장 이름', '예) 스타벅스'),
+                    textFormWidget('hourlywage', '시급', '예) 9,160'),
+                    firstDay(),
+                    const Text(
+                      '근무 시간',
+                      style: headstyle,
+                    ),
+                    empty(),
+                    Row(
                       children: [
-                        selectWork('음식·음료'),
-                        selectWork('유통·판매'),
-                        selectWork('문화·여가'),
-                        selectWork('서비스'),
+                        workTime('attendance', '출근 시간'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        workTime('closing', '퇴근 시간'),
                       ],
                     ),
-                  ),
-                  inputInfo('직장 이름', '예) 스타벅스', null),
-                  inputInfo(
-                    '첫 출근일',
-                    '예)2022-06-27',
-                    GestureDetector(
-                      onTap: () {
-                        showDatePicker(
-                            builder: (context, child) {
-                              return Theme(
-                                data: ThemeData.dark().copyWith(
-                                  colorScheme: const ColorScheme.light(
-                                    primary: pointcolor,
-                                    onPrimary: Colors.white,
-                                    surface: Colors.white,
-                                    onSurface: pointcolor,
-                                  ),
-                                  dialogBackgroundColor: Colors.white,
-                                ),
-                                child: child!,
-                              );
-                            },
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2020, 06, 28),
-                            lastDate: DateTime(2024, 06, 28));
-                      },
-                      child: const Icon(
-                        Icons.calendar_today_rounded,
-                        color: pointcolor,
-                      ),
+                    const SizedBox(
+                      height: 50,
                     ),
-                  ),
-                  const Text(
-                    '근무 시간',
-                    style: headstyle,
-                  ),
-                  empty(),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(
-                              5,
-                            ),
-                          ),
-                          child: TextFormField(
-                            initialValue:
-                                _.time == null ? null : _.time.toString(),
-                            cursorWidth: 1,
-                            cursorColor: Colors.black,
-                            style: headstyle,
-                            decoration: InputDecoration(
-                              suffixIcon: GestureDetector(
-                                onTap: () async {
-                                  TimeOfDay? newTime = await showTimePicker(
-                                      builder: (context, child) {
-                                        return Theme(
-                                          data: ThemeData.dark().copyWith(
-                                            colorScheme:
-                                                const ColorScheme.light(
-                                              primary: pointcolor,
-                                              onPrimary: Colors.white,
-                                              surface: Colors.white,
-                                              onSurface: pointcolor,
-                                            ),
-                                            dialogBackgroundColor:
-                                                Colors.green[900],
-                                          ),
-                                          child: child!,
-                                        );
-                                      },
-                                      hourLabelText: '',
-                                      errorInvalidText: '다시 입력해주세요',
-                                      minuteLabelText: '',
-                                      cancelText: '취소',
-                                      confirmText: '확인',
-                                      helpText: null,
-                                      initialEntryMode:
-                                          TimePickerEntryMode.input,
-                                      context: context,
-                                      initialTime: TimeOfDay.now());
-                                  if (newTime != null) _.changeTime(newTime);
-                                  print(_.time);
-                                },
-                                child: const Icon(
-                                  CupertinoIcons.clock,
-                                  color: pointcolor,
-                                ),
-                              ),
-                              suffixStyle: headstyle,
-                              border: InputBorder.none,
-                              contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 10),
-                              hoverColor: Colors.white,
-                              hintText: '출근 시간',
-                              hintStyle: TextStyle(
-                                color: Colors.black.withOpacity(0.2),
-                              ),
-                            ),
-                          ),
+                    InkWell(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
                         ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(
-                              5,
-                            ),
-                          ),
-                          child: TextFormField(
-                            cursorWidth: 1,
-                            cursorColor: Colors.black,
-                            style: headstyle,
-                            decoration: InputDecoration(
-                              suffixIcon: const Icon(
-                                CupertinoIcons.clock,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                '작성 완료',
+                                style: TextStyle(
+                                    color: pointcolor,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Icon(
+                                Icons.check_rounded,
                                 color: pointcolor,
                               ),
-                              suffixStyle: headstyle,
-                              border: InputBorder.none,
-                              contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 10),
-                              hoverColor: Colors.white,
-                              hintText: '퇴근 시간',
-                              hintStyle: TextStyle(
-                                color: Colors.black.withOpacity(0.2),
-                              ),
-                            ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  empty(),
-                  inputInfo('시급', '예) 9,160', null),
-                  empty(),
-                  empty(),
-                  empty(),
-                  InkWell(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.white,
-                      ),
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              '작성 완료',
-                              style: headstyle,
-                            ),
-                            const Icon(
-                              Icons.check_rounded,
-                              color: Colors.green,
-                            ),
-                          ],
-                        ),
-                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -242,19 +102,166 @@ class AddTaskPage extends StatelessWidget {
     );
   }
 
-  Widget selectWork(String work) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.white,
-      ),
-      child: Center(
-        child: Text(work, style: pointstyle),
+  SizedBox empty() {
+    return const SizedBox(
+      height: 10,
+    );
+  }
+
+  Widget choiceJob() {
+    return GetBuilder<JobController>(builder: (_) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            '직종 선택',
+            style: headstyle,
+          ),
+          empty(),
+          SizedBox(
+            height: 50,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                childAspectRatio: 2 / 1,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    onTap: () {
+                      _.changejobIndex(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: _.jobindex == null
+                            ? null
+                            : _.jobindex == index
+                                ? Border.all(color: pointcolor, width: 2)
+                                : null,
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Text(_.typeofjob[index], style: pointstyle),
+                      ),
+                    ));
+              },
+              itemCount: _.typeofjob.length,
+            ),
+          ),
+        ],
+      );
+    });
+  }
+
+  Widget workTime(String name, String hinttext) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(
+            5,
+          ),
+        ),
+        child: FormBuilderDateTimePicker(
+          format: DateFormat.Hm(),
+          transitionBuilder: (context, child) {
+            return Theme(
+              data: ThemeData.dark().copyWith(
+                primaryColor: pointcolor,
+                colorScheme: const ColorScheme.light(
+                  primary: pointcolor,
+                  surface: Colors.white,
+                  onSurface: pointcolor,
+                ),
+                dialogBackgroundColor: Colors.white,
+              ),
+              child: child!,
+            );
+          },
+          inputType: InputType.time,
+          timePickerInitialEntryMode: TimePickerEntryMode.input,
+          name: name,
+          style: headstyle,
+          decoration: InputDecoration(
+            suffixIcon: const Icon(
+              CupertinoIcons.clock,
+              color: pointcolor,
+            ),
+            border: InputBorder.none,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            hoverColor: Colors.white,
+            hintText: hinttext,
+            hintStyle: TextStyle(
+              color: Colors.black.withOpacity(0.2),
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  Widget inputInfo(String title, String hinttext, GestureDetector? icon) {
+  Widget firstDay() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          '첫 출근일',
+          style: headstyle,
+        ),
+        empty(),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              5,
+            ),
+          ),
+          child: FormBuilderDateTimePicker(
+            locale: const Locale('ko', 'KR'),
+            inputType: InputType.date,
+            format: DateFormat.yMMMEd('ko'),
+            name: 'firstday',
+            style: headstyle,
+            transitionBuilder: (context, child) {
+              return Theme(
+                data: ThemeData.dark().copyWith(
+                  colorScheme: const ColorScheme.light(
+                    primary: pointcolor,
+                    onPrimary: Colors.white,
+                    surface: Colors.white,
+                    onSurface: pointcolor,
+                  ),
+                  dialogBackgroundColor: Colors.white,
+                ),
+                child: child!,
+              );
+            },
+            decoration: InputDecoration(
+              suffixIcon: const Icon(
+                Icons.calendar_today_rounded,
+                color: pointcolor,
+              ),
+              suffixStyle: headstyle,
+              border: InputBorder.none,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              hoverColor: Colors.white,
+              hintText: '예) ${DateFormat.yMMMMd('ko').format(DateTime.now())}',
+              hintStyle: TextStyle(
+                color: Colors.black.withOpacity(0.2),
+              ),
+            ),
+          ),
+        ),
+        empty()
+      ],
+    );
+  }
+
+  Widget textFormWidget(String name, String title, String hinttext) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -271,13 +278,11 @@ class AddTaskPage extends StatelessWidget {
             ),
           ),
           child: FormBuilderTextField(
-            name: 'jobname',
+            name: name,
             cursorWidth: 1,
             cursorColor: Colors.black,
             style: headstyle,
             decoration: InputDecoration(
-              suffixIcon: icon,
-              suffixStyle: headstyle,
               border: InputBorder.none,
               contentPadding:
                   const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -291,12 +296,6 @@ class AddTaskPage extends StatelessWidget {
         ),
         empty()
       ],
-    );
-  }
-
-  SizedBox empty() {
-    return const SizedBox(
-      height: 10,
     );
   }
 }
