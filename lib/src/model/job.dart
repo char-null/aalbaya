cca13ae5be6f892bfd2fb2f9bbd56f1cd258d0ea
char-null <wool999999@gmail.com>
2022-06-28 22:02:55 +0900

    job 모델 생성

---
+++ lib/src/model/job.dart
@@ -0,0 +1,24 @@
+// class Job {
+//   final String taskname;
+//   final String attendandce;
+//   final String uploaddate;
+//   final String hashtag;
+//   Task(
+//       {required this.taskname,
+//       required this.attendandce,
+//       required this.uploaddate,
+//       required this.hashtag});
+
+//   factory Job.fromMap(Map<String, dynamic> json) => Job(
+//       content: json['content'],
+//       title: json['title'],
+//       uploaddate: json['uploaddate'],
+//       hashtag: json['hashtag']);
+
+//   Map<String, dynamic> toMap() => {
+//         "title": title,
+//         "content": content,
+//         "uploaddate": uploaddate,
+//         "hashtag": hashtag
+//       };
+// }
