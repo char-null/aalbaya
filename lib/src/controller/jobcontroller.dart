3c2c45a5fa9f67671370bda8e6f89d1c676358d9
char-null <wool999999@gmail.com>
2022-06-30 22:31:37 +0900

    jobcontroller 수정

--- lib/src/controller/jobcontroller.dart
+++ lib/src/controller/jobcontroller.dart
@@ -3,18 +3,42 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
 class JobController extends GetxController {
-  TimeOfDay? time;
   final formkey = GlobalKey<FormBuilderState>();
   List<String> typeofjob = ['음식·음료', '유통·판매', '문화·여가', '기타'];
   int? jobindex;
+  bool choicejobvalidate = true;
+  bool dayselectedvalidate = true;
+  List<String> dayofWeek = ['월', '화', '수', '목', '금', '토', '일'];
+  List<bool> dayselected = [false, false, false, false, false, false, false];
 
-  changeTime(TimeOfDay newTime) {
-    time = newTime;
+  void changejobIndex(int index) {
+    jobindex = index;
     update();
   }
 
-  changejobIndex(int index) {
-    jobindex = index;
+  void daySelected(int index) {
+    dayselected[index] == false
+        ? dayselected[index] = true
+        : dayselected[index] = false;
+    update();
+  }
+
+  void choicejobValidator() {
+    jobindex == null ? choicejobvalidate = false : choicejobvalidate = true;
+    update();
+  }
+
+  void dayselectedValidator() {
+    // var j = 0;
+    // for (var i = 0; i < dayselected.length; i++) {
+    //   if (dayselected[i] == true) {
+    //     j++;
+    //   }
+    // }
+    // j > 0 ? dayselectedvalidate = true : dayselectedvalidate = false;
+    dayselected.where((e) => e == false).length == 7
+        ? dayselectedvalidate = false
+        : dayselectedvalidate = true;
     update();
   }
 }
