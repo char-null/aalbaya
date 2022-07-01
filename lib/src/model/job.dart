c656c7c64f0eb99e517f252aadefead039904690
char-null <wool999999@gmail.com>
2022-07-01 22:34:12 +0900

    job model 생성

--- lib/src/model/job.dart
+++ lib/src/model/job.dart
@@ -1,24 +1,57 @@
-// class Job {
-//   final String taskname;
-//   final String attendandce;
-//   final String uploaddate;
-//   final String hashtag;
-//   Task(
-//       {required this.taskname,
-//       required this.attendandce,
-//       required this.uploaddate,
-//       required this.hashtag});
+// To parse this JSON data, do
+//
+//     final job = jobFromJson(jsonString);
 
-//   factory Job.fromMap(Map<String, dynamic> json) => Job(
-//       content: json['content'],
-//       title: json['title'],
-//       uploaddate: json['uploaddate'],
-//       hashtag: json['hashtag']);
+import 'dart:convert';
 
-//   Map<String, dynamic> toMap() => {
-//         "title": title,
-//         "content": content,
-//         "uploaddate": uploaddate,
-//         "hashtag": hashtag
-//       };
-// }
+Job jobFromJson(String str) => Job.fromMap(json.decode(str));
+
+String jobToJson(Job data) => json.encode(data.toMap());
+
+class Job {
+  Job({
+    required this.jobtype,
+    required this.jobdetail,
+    required this.jobname,
+    required this.hourlywage,
+    required this.firstday,
+    required this.workday,
+    required this.attendance,
+    required this.closing,
+    required this.ing,
+  });
+
+  String jobtype;
+  String jobdetail;
+  String jobname;
+  int hourlywage;
+  String firstday;
+  List<String> workday;
+  String attendance;
+  String closing;
+  bool ing;
+
+  factory Job.fromMap(Map<String, dynamic> json) => Job(
+        jobtype: json["jobtype"],
+        jobdetail: json["jobdetail"],
+        jobname: json["jobname"],
+        hourlywage: json["hourlywage"],
+        firstday: json["firstday"],
+        workday: List<String>.from(json["workday"].map((x) => x)),
+        attendance: json["attendance"],
+        closing: json["closing"],
+        ing: json["ing"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "jobtype": jobtype,
+        "jobdetail": jobdetail,
+        "jobname": jobname,
+        "hourlywage": hourlywage,
+        "firstday": firstday,
+        "workday": List<dynamic>.from(workday.map((x) => x)),
+        "attendance": attendance,
+        "closing": closing,
+        "ing": ing,
+      };
+}
