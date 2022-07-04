fb931e3cefed20af8581cd2ec3904c32570922e8
char-null <wool999999@gmail.com>
2022-07-04 23:47:47 +0900

    controller 수정

--- lib/src/controller/viewcontroller.dart
+++ lib/src/controller/viewcontroller.dart
@@ -1,27 +1,18 @@
 import 'package:aalbaya/src/db/db.dart';
 import 'package:aalbaya/src/model/job.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
 class ViewController extends GetxController {
   late List<Job>? jobdata;
   late List<Job>? ongoingjob;
   late List<Job>? lastjob;
+  final formKey = GlobalKey<FormBuilderState>();
   Future<void> jobData() async {
     jobdata = await DatabaseHelper.instance.getJob();
+    ongoingjob = jobdata!.where((e) => e.ing == 'true').toList();
+    lastjob = jobdata!.where((e) => e.ing == 'false').toList();
     update();
-  }
-
-  void ingType() {
-    for (var i = 0; i < jobdata!.length; i++) {
-      if (jobdata![i].ing == 'true') {
-        ongoingjob![i] = jobdata![i];
-        update();
-      } else {
-        for (var j = 0; j < jobdata!.length; j++) {
-          lastjob![j] = jobdata![i];
-          update();
-        }
-      }
-    }
   }
 }
