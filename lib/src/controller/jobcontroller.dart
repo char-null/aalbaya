38bbcb084d57d7c9a444f0b9e21a4d85cb30de67
char-null <wool999999@gmail.com>
2022-07-04 23:47:35 +0900

    controller 수정

--- lib/src/controller/jobcontroller.dart
+++ lib/src/controller/jobcontroller.dart
@@ -6,7 +6,6 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
 class JobController extends GetxController {
-  final formkey = GlobalKey<FormBuilderState>();
   List<String> typeofjob = ['음식·음료', '유통·판매', '문화·여가', '기타'];
   int? jobindex;
   bool choicejobvalidate = true;
