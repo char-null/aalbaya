b674fdf8098119312ab0d166c04ef51c8f5d6b60
char-null <wool999999@gmail.com>
2022-07-05 23:44:18 +0900

    날짜 계산 추가

--- lib/src/pages/test_page.dart
+++ lib/src/pages/test_page.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:aalbaya/src/controller/viewcontroller.dart';
+import 'package:aalbaya/src/db/db.dart';
+import 'package:aalbaya/src/model/job.dart';
 import 'package:aalbaya/src/styles/colors.dart';
 import 'package:aalbaya/src/styles/textstyle.dart';
 import 'package:flutter/material.dart';
@@ -27,17 +29,52 @@ class TestPage extends StatelessWidget {
             )),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 18.0),
-        child: GetBuilder<ViewController>(builder: (_) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              onGoingJob(_),
-              lasJob(_),
-            ],
-          );
-        }),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: GetBuilder<ViewController>(builder: (_) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                onGoingJob(_),
+                empty(),
+                lastJob(_),
+                empty(),
+                empty(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      '통계',
+                      style: headstyle,
+                    ),
+                    empty(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.8),
+                            spreadRadius: 0.4,
+                            blurRadius: 2,
+                            offset: const Offset(0, 4),
+                          )
+                        ],
+                      ),
+                      child: Text(
+                        '지금까지 총 ${_.jobdata!.length}개의 직장에서 ',
+                        style: contentstyle,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            );
+          }),
+        ),
       ),
     );
   }
@@ -48,7 +85,7 @@ class TestPage extends StatelessWidget {
     );
   }
 
-  Widget lasJob(ViewController _) {
+  Widget lastJob(ViewController _) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -85,40 +122,59 @@ class TestPage extends StatelessWidget {
                 physics: const ScrollPhysics(),
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  return Container(
-                    height: 100,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.8),
-                          spreadRadius: 0.4,
-                          blurRadius: 2,
-                          offset: const Offset(0, 4),
-                        )
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _.lastjob![index].jobname,
-                          style: headstyle,
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.8),
+                              spreadRadius: 0.4,
+                              blurRadius: 2,
+                              offset: const Offset(0, 4),
+                            )
+                          ],
                         ),
-                        empty(),
-                        Text(
-                          '직종 : ${_.lastjob![index].jobdetail}',
-                          style: contentstyle,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _.lastjob![index].jobname,
+                              style: headstyle,
+                            ),
+                            empty(),
+                            Text(
+                              '직종 : ${_.lastjob![index].jobtype}',
+                              style: contentstyle,
+                            ),
+                            Text(
+                              '세부직종 : ${_.lastjob![index].jobdetail}',
+                              style: contentstyle,
+                            ),
+                            Text(
+                              '시급 : ${_.lastjob![index].hourlywage}',
+                              style: contentstyle,
+                            ),
+                            Text(
+                              '알바기간 : ${DateFormat('yy-MM-dd', 'ko').format(DateTime.parse(_.lastjob![index].firstday))} ~ ${DateFormat('yy-MM-dd', 'ko').format(DateTime.parse(_.lastjob![index].closeday!))}',
+                              style: contentstyle,
+                            ),
+                            Text(
+                              '실제 근무일수 : ${_.lastjob![index].totalday}일',
+                              style: contentstyle,
+                            ),
+                            Text('근무 요일 : ${_.lastjob![index].workday}')
+                          ],
                         ),
-                        Text(
-                          '시급 : ${_.lastjob![index].hourlywage}',
-                          style: contentstyle,
-                        ),
-                      ],
-                    ),
+                      ),
+                      empty(),
+                      empty(),
+                    ],
                   );
                 }),
       ],
@@ -141,6 +197,7 @@ class TestPage extends StatelessWidget {
                 itemCount: _.ongoingjob!.length,
                 itemBuilder: (context, index) {
                   List daylist = jsonDecode(_.ongoingjob![index].workday);
+
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
@@ -173,16 +230,16 @@ class TestPage extends StatelessWidget {
                                   onTap: () {
                                     Get.dialog(
                                       AlertDialog(
-                                        title: Text(
+                                        title: const Text(
                                           '그만두기',
-                                          style: TextStyle(),
+                                          style: const TextStyle(),
                                         ),
                                         content: Column(
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           mainAxisSize: MainAxisSize.min,
                                           children: [
-                                            Text('그만두는 날짜'),
+                                            const Text('그만두는 날짜'),
                                             FormBuilder(
                                               key: _.formKey,
                                               child: FormBuilderDateTimePicker(
@@ -194,7 +251,7 @@ class TestPage extends StatelessWidget {
                                                 format: DateFormat(
                                                     'yy-MM-dd', 'ko'),
                                                 inputType: InputType.date,
-                                                name: 'close',
+                                                name: 'closeday',
                                                 decoration: InputDecoration(
                                                   hintText: DateFormat(
                                                           'yy-MM-dd', 'ko')
@@ -218,21 +275,98 @@ class TestPage extends StatelessWidget {
                                               onTap: () {
                                                 Get.back();
                                               },
-                                              child: Text(
+                                              child: const Text(
                                                 '취소',
                                                 style: contentpointstyle,
                                               )),
                                           GestureDetector(
-                                              onTap: () {
+                                              onTap: () async {
                                                 if (_.formKey.currentState!
                                                     .validate()) {
                                                   _.formKey.currentState!
                                                       .save();
-                                                  print(_.formKey.currentState!
-                                                      .value);
+                                                  var total = DateTime.parse(_
+                                                          .formKey
+                                                          .currentState!
+                                                          .value['closeday']
+                                                          .toString())
+                                                      .difference(
+                                                          DateTime.parse(_
+                                                              .ongoingjob![
+                                                                  index]
+                                                              .firstday));
+                                                  _.totalDay(
+                                                      jsonDecode(_
+                                                          .ongoingjob![index]
+                                                          .workday),
+                                                      total.inDays);
+                                                  await DatabaseHelper.instance
+                                                      .updatecloseJob(
+                                                          Job(
+                                                              id: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .id,
+                                                              jobtype: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .jobtype,
+                                                              jobdetail: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .jobdetail,
+                                                              jobname: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .jobname,
+                                                              hourlywage: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .hourlywage,
+                                                              firstday: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .firstday,
+                                                              workday: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .workday,
+                                                              attendance: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .attendance,
+                                                              closing: _
+                                                                  .ongoingjob![
+                                                                      index]
+                                                                  .closing,
+                                                              closeday: _
+                                                                  .formKey
+                                                                  .currentState!
+                                                                  .value[
+                                                                      'closeday']
+                                                                  .toString(),
+                                                              totalday:
+                                                                  _.totalday,
+                                                              ing: 'false'),
+                                                          _.ongoingjob![index]
+                                                              .id);
+                                                  _.jobData();
+                                                  Get.back();
+                                                  Get.snackbar(
+                                                    '그만 두기 완료',
+                                                    '알바를 그만두었어요!',
+                                                    snackPosition:
+                                                        SnackPosition.BOTTOM,
+                                                    icon: Icon(
+                                                      Icons.check,
+                                                      color: pointcolor,
+                                                    ),
+                                                    duration: Duration(
+                                                        milliseconds: 1000),
+                                                  );
                                                 }
                                               },
-                                              child: Text(
+                                              child: const Text(
                                                 '확인',
                                                 style: contentpointstyle,
                                               ))
@@ -240,7 +374,7 @@ class TestPage extends StatelessWidget {
                                       ),
                                     );
                                   },
-                                  child: Text(
+                                  child: const Text(
                                     '그만두기',
                                     style: pointstyle,
                                   ),
@@ -298,8 +432,12 @@ class TestPage extends StatelessWidget {
                   ],
                 ),
                 child: const Center(
-                  child: const Text('진행중인 알바가 없어요!'),
-                )),
+                  child: Text(
+                    '진행중인 알바가 없어요!',
+                    style: contentpointstyle,
+                  ),
+                ),
+              ),
       ],
     );
   }
