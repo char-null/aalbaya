50508d39b9cb0518e57ef789a7dbbb83f8753ae7
char-null <wool999999@gmail.com>
2022-07-10 23:59:27 +0900

    size 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:aalbaya/src/pages/home_page.dart';
+import 'package:aalbaya/src/styles/colors.dart';
+import 'package:aalbaya/src/styles/textstyle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -12,18 +15,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        // if it's a RTL language
-      ],
-      supportedLocales: [
-        Locale('ko', 'KR'),
-        // include country code too
-      ],
-      home: HomePage(),
+    return ScreenUtilInit(
+      designSize: const Size(1080, 2340),
+      builder: (context, child) {
+        return GetMaterialApp(
+          builder: (context, child) {
+            return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: child!);
+          },
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            // if it's a RTL language
+          ],
+          supportedLocales: const [
+            Locale('ko', 'KR'),
+
+            // include country code too
+          ],
+          debugShowCheckedModeBanner: false,
+          home: const HomePage(),
+          theme: ThemeData(
+            appBarTheme: AppBarTheme(
+              titleTextStyle: TextStyle(
+                  fontSize: textsize * 0.9,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              elevation: 0,
+              backgroundColor: pointcolor,
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/src/styles/textstyle.dart
+++ lib/src/styles/textstyle.dart
@@ -1,13 +1,32 @@
 import 'package:aalbaya/src/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-const headstyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+final double width = 540.w;
+final double height = 1100.h;
+final double textsize = 60.sp;
 
-const subheadstyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+final headstyle = TextStyle(
+    fontSize: textsize, fontWeight: FontWeight.bold, color: Colors.black);
 
-const contentstyle = TextStyle(fontSize: 16, color: Colors.black);
+final subheadstyle =
+    TextStyle(fontSize: textsize * 0.85, fontWeight: FontWeight.bold);
 
-const contentpointstyle =
-    TextStyle(fontSize: 16, color: pointcolor, fontWeight: FontWeight.bold);
+final contentstyle = TextStyle(fontSize: textsize * 0.75, color: Colors.black);
 
-const pointstyle = TextStyle(fontWeight: FontWeight.bold, color: pointcolor);
+final contentpointstyle = TextStyle(
+    fontSize: textsize * 0.75, color: pointcolor, fontWeight: FontWeight.bold);
+
+final pointstyle = TextStyle(
+    fontWeight: FontWeight.bold, color: pointcolor, fontSize: textsize * 0.75);
+
+final formbuilderstyle = TextStyle(fontSize: textsize * 0.85, height: 1);
+
+final formbuilderhintstyle = TextStyle(
+    fontSize: textsize * 0.85, color: Colors.black.withOpacity(0.2), height: 1);
+
+final listtextstyle = TextStyle(
+  fontSize: textsize * 0.74,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
