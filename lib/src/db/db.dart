25f31ade0484191926e7bee7fa7763a155c68e6f
char-null <wool999999@gmail.com>
2022-07-11 00:00:05 +0900

    const

--- lib/src/db/db.dart
+++ lib/src/db/db.dart
@@ -5,7 +5,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-final String tablename = 'job';
+const String tablename = 'job';
 
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
